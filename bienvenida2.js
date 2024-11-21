d908bd31bb3a37d2a2176a6ab9fcce8b33f25191
Max1 <macrocce@uade.edu.ar>
2024-11-21 20:43:33 -0300

    Add files via upload

---
+++ bienvenida2.js
@@ -0,0 +1,5 @@
+var nombre = prompt("Por favor, ingresa tu nombre:");
+var apellido = prompt("Por favor, ingresa tu apellido:");
+var edad = prompt("Por favor, ingresa tu edad:");
+
+alert("Hola " + nombre + " " + apellido + ", bienvenido, tienes " + edad + " años");
